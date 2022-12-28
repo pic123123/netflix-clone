5313abf1dda7348360a66871222be6a55d83143a
Joker <40009719+pic123123@users.noreply.github.com>
2022-12-28 12:41:58 +0900

    1. basicheader

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Home() {
-  return <div>Home</div>;
+  return <div style={{ backgroundColor: "whitesmoke", height: "200vh" }}></div>;
 }
 
 export default Home;
